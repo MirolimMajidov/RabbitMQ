7a4db3a7e5b44fefadcf48eb05c7421afcc602b0
Mirolim Majidov <majidovmirolimjon@gmail.com>
2021-05-03 09:22:00 +0500

    Created projects for hello world example

---
+++ HelloWorld/Send/Send.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                }
+            }
+        }
+    }
+}
