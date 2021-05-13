f7bb059667982eeb1b5014d8bf93e8f8c56b8ebc
Mirolim Majidov <majidovmirolimjon@gmail.com>
2021-05-13 19:53:04 +0500

    finished first example

--- HelloWorld/Receive/Receive.cs
+++ HelloWorld/Receive/Receive.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace Receive
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "Hi", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Received: {0}", message);
+                };
+                channel.BasicConsume(queue: "Hi", autoAck: true, consumer: consumer);
+            }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
--- HelloWorld/Send/Send.cs
+++ HelloWorld/Send/Send.cs
@@ -10,12 +10,24 @@ namespace Send
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
             {
-                using (var channel = connection.CreateModel())
-                {
+                channel.QueueDeclare(queue: "Hi", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
+                for (int i = 1; i < 11; i++)
+                    SendMessage($"{i}, Hi Benom!");
+
+                void SendMessage(string message)
+                {
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "", routingKey: "Hi", basicProperties: null, body: body);
+
+                    Console.WriteLine("Sent: {0}", message);
                 }
             }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
