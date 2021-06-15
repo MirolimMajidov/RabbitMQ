ebcf18c1da3cead5c26d52e07c655d715fc2a55f
Mirolim Majidov <Mirolim@silkroadprofessionals.com>
2021-06-15 14:05:50 +0500

    created projects for routing Publish/Subscribe

---
+++ Publish_Subscribe/PublisherService/Receive.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using EventBus.RabbitMQ;
+
+namespace SubscribeService
+{
+    public class Receive
+    {
+        public static IEventBusRabbitMQ EventBus { get; set; }
+
+        public static void Main()
+        {
+            EventBus = new EventBusRabbitMQ();
+            EventBus.Model.ExchangeDeclare(exchange: EventBus.Exchange, type: ExchangeType.Fanout);
+
+            var routingKey = string.Empty;
+            var queueName = EventBus.Model.QueueDeclare().QueueName;
+            EventBus.Model.QueueBind(queue: queueName, exchange: EventBus.Exchange, routingKey: routingKey);
+
+            Console.WriteLine("[*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(EventBus.Model);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine("[x] Received '{0}':'{1}'", routingKey, message);
+            };
+            EventBus.Model.BasicConsume(queue: queueName,  autoAck: true, consumer: consumer);
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Publish_Subscribe/SubscribeService/Send.cs
@@ -0,0 +1,36 @@
+﻿using EventBus.RabbitMQ;
+using RabbitMQ.Client;
+using System;
+
+namespace PublisherService
+{
+    public class Send
+    {
+        static int MessageId = 1;
+        public static IEventBusRabbitMQ EventBus { get; set; }
+
+        public static void Main(string[] args)
+        {
+            EventBus = new EventBusRabbitMQ();
+            EventBus.Model.ExchangeDeclare(exchange: EventBus.Exchange, type: ExchangeType.Fanout);
+
+            SendMessage();
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        static void SendMessage()
+        {
+            Console.Write($"Enter your {MessageId} message:");
+            var message = Console.ReadLine();
+
+            EventBus.Publish(message, routingKey: "");
+            Console.WriteLine("[x] Sent:'{0}'", message);
+
+            MessageId++;
+
+            SendMessage();
+        }
+    }
+}
